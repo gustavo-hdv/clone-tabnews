d3b62abbe69ac15b06198984e3d912a8f8294e28
Gustavo H. <gustavohenrique247@hotmail.com>
2025-02-23 22:54:10 +0000

    index change

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Chocolate é muito bom!</h1>
-}
+    return (
+      <div className="flex items-center justify-center h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+        <h1 className="text-4xl font-bold text-white shadow-lg p-4 rounded-xl">
+          Chocolate é muito bom!
+        </h1>
+      </div>
+    );
+  }
 
 export default Home;
